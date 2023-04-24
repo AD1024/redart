5e3d53755be4a3cf6e19826fae86cbdeb4e73639
AD1024 <dh63@cs.washington.edu>
2023-04-24 14:33:45 -0400

    [add] missing file

---
+++ redart/simulator/naive_sim.py
@@ -0,0 +1,25 @@
+from redart.data import Packet
+from redart.simulator.traits import SimulatorTrait
+
+
+class NaiveSimulator(SimulatorTrait):
+    def __init__(self):
+        super().__init__(None, {}, name="NaiveSimulator")
+        self.record = {}
+        self.rtt_samples = {}
+
+    # A reference rather than ground truth
+    # When retransmission, we pessimistically take with the first SEQ packet
+    def process_packet(self, packet: Packet):
+        key = packet.to_src_dst_key()
+        if packet.is_seq():
+            eack = packet.seq + packet.packet_size
+            if (key, eack) not in self.record:
+                self.record[key, eack] = packet
+        elif packet.is_ack():
+            if (key, packet.ack) in self.record:
+                if key not in self.rtt_samples:
+                    self.rtt_samples[key] = []
+                self.rtt_samples[key].append(
+                    packet.timestamp - self.record[key, packet.ack].timestamp)
+                self.record.pop((key, packet.ack))
